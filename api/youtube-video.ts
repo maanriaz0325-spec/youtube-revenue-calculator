57d6ddf35795e6fd6b569ef8c0056213e382ef37
Amina Riaz <maanriaz0325@gmail.com>
2026-06-19 01:49:15 +0500

    fix vercel types import

--- api/youtube-video.ts
+++ api/youtube-video.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { GoogleGenAI, Type } from "@google/genai";
 import { calculateRevenue } from "../src/lib/youtubeLogic.js";
 const ai = new GoogleGenAI({
